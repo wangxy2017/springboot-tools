c93a2f13361851ddc816f5f0f29be3058b022ba5
wangxiaoyuan <243548880@qq.com>
2020-01-15 13:59:38 +0800

    init

---
+++ src/main/java/com/example/springboot/tools/SpringbootToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/tools/SpringbootToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
